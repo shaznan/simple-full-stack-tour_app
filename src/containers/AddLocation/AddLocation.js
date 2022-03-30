16f8e0acf9154dccc3dc3c302451843076f52c39
shaznan123 <shaznanfairozesally@gmail.com>
2022-03-30 20:23:44 +0530

    refactor

---
+++ src/containers/AddLocation/AddLocation.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import AddLocationModal from "../../components/AddLocationModal/AddLocationModal";
+import AddBtn from "../../components/AddBtn";
+
+function AddLocation() {
+  const [firstName, setFirstName] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [imgUrl, setImgUrl] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(false);
+
+  const [open, setOpen] = useState(true);
+  const handleClose = () => setOpen(false);
+  const handleOpen = () => setOpen(true);
+
+  const onSubmitHandler = () => {
+    !firstName || !title || !imgUrl || !description || !location
+      ? setErrorMsg(true)
+      : setErrorMsg(false);
+  };
+  return (
+    <>
+      <AddBtn handleOpen={handleOpen} />
+      <AddLocationModal
+        setFirstName={setFirstName}
+        setTitle={setTitle}
+        setImgUrl={setImgUrl}
+        setDescription={setDescription}
+        setLocation={setLocation}
+        errorMsg={errorMsg}
+        open={open}
+        handleClose={handleClose}
+        onSubmitHandler={onSubmitHandler}
+      />
+    </>
+  );
+}
+
+export default AddLocation;
