979c92a8bd3f368e9301e0158c8690c7df3d058e
shaznan123 <shaznanfairozesally@gmail.com>
2022-04-11 23:34:48 +0530

    implement app login

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import TourScreen from "./screens/TourScreen";
 import { Routes, Route, Link } from "react-router-dom";
 import Register from "./screens/Register";
+import Login from "./screens/Login";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Register />} />
+      <Route path="/Login" element={<Login />} />
       <Route path="tours" element={<TourScreen />} />
     </Routes>
   );
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
+import UserPool from "../Auth/userPool";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const user = new CognitoUser({
+      Username: email,
+      Pool: UserPool,
+    });
+
+    const authDetails = new AuthenticationDetails({
+      Username: email,
+      Password: password,
+    });
+
+    user.authenticateUser(authDetails, {
+      onSuccess: (data) => {
+        console.log("onSuccess: ", data);
+      },
+      onFailure: (err) => {
+        console.error("onFailure: ", err);
+      },
+      newPasswordRequired: (data) => {
+        console.log("newPasswordRequired: ", data);
+      },
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        ></input>
+        <label htmlFor="password">Password</label>
+        <input
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        ></input>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
